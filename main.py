a241bb24c345f3830f21c6c1bb183cb71c2ecc14
kmckoy14 <keatonsport@gmail.com>
2025-01-06 14:23:07 -0800

    added main and read_book functions to main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+
+# Entry Point Function
+
+def main():
+    # reading book file
+    book_file_path = "books/frankenstein.txt"
+    read = read_books(book_file_path)
+    print(read)
+
+def read_books(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
+# python3 main.py
