3d60d6997ef65c8366a2695d7dab42b6ba287de6
kmckoy14 <keatonsport@gmail.com>
2025-01-06 14:37:29 -0800

    added a counting words function to main.py

--- main.py
+++ main.py
@@ -4,8 +4,18 @@
 def main():
     # reading book file
     book_file_path = "books/frankenstein.txt"
-    read = read_books(book_file_path)
-    print(read)
+    text = read_books(book_file_path)
+
+    # Count Words
+    word_count = get_word_count(text)
+    
+
+def get_word_count(book_text):
+    words = book_text.split()
+    count_words = len(words)
+    return count_words
+
+
 
 def read_books(path):
     with open(path) as f:
